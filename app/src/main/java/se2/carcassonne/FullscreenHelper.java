a5721b99accd028f93d4b4c8c816be84c355285e
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-03-29 10:51:17 +0100

    feat: Added Homescreen and Responsive Design
    Renamed Activites. Added Homescreen Activity with Background. Made Startupscreen Responsive and on button click move to Homescreen.

---
+++ app/src/main/java/se2/carcassonne/FullscreenHelper.java
@@ -0,0 +1,13 @@
+package se2.carcassonne;
+
+import android.app.Activity;
+import android.view.View;
+
+public class FullscreenHelper {
+    public static void setFullscreenAndImmersiveMode(Activity activity) {
+        activity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+}
---
+++ app/src/main/java/se2/carcassonne/HomeActivity.java
@@ -0,0 +1,14 @@
+package se2.carcassonne;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        FullscreenHelper.setFullscreenAndImmersiveMode(this);
+    }
+}
