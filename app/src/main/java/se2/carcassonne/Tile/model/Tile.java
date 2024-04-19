9f889f7fc524095e18e92a4b1377e2721142ea03
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2024-04-19 16:12:46 +0200

    feat: create Tile class with all getters and setters

---
+++ app/src/main/java/se2/carcassonne/Tile/model/Tile.java
@@ -0,0 +1,72 @@
+package se2.carcassonne.Tile.model;
+
+public class Tile {
+    int tileId;
+
+    String tileName;
+
+//    TODO: check how to access the image resource int or String?
+    int tileImage;
+    String northEdgeType;
+    String southEdgeType;
+    String eastEdgeType;
+    String westEdgeType;
+
+    public int getTileId() {
+        return tileId;
+    }
+
+    public void setTileId(int tileId) {
+        this.tileId = tileId;
+    }
+
+    public String getTileName() {
+        return tileName;
+    }
+
+    public void setTileName(String tileName) {
+        this.tileName = tileName;
+    }
+
+    public int getTileImage() {
+        return tileImage;
+    }
+
+    public void setTileImage(int tileImage) {
+        this.tileImage = tileImage;
+    }
+
+    public String getNorthEdgeType() {
+        return northEdgeType;
+    }
+
+    public void setNorthEdgeType(String northEdgeType) {
+        this.northEdgeType = northEdgeType;
+    }
+
+    public String getSouthEdgeType() {
+        return southEdgeType;
+    }
+
+    public void setSouthEdgeType(String southEdgeType) {
+        this.southEdgeType = southEdgeType;
+    }
+
+    public String getEastEdgeType() {
+        return eastEdgeType;
+    }
+
+    public void setEastEdgeType(String eastEdgeType) {
+        this.eastEdgeType = eastEdgeType;
+    }
+
+    public String getWestEdgeType() {
+        return westEdgeType;
+    }
+
+    public void setWestEdgeType(String westEdgeType) {
+        this.westEdgeType = westEdgeType;
+    }
+}
+
+
