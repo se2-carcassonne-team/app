450dbba04cd4866f9d67e6a0d38e286bcdee5106
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-04-25 14:34:01 +0200

    refact: Remove unused Methods in Player.java
    toString now handled via Lombok

--- app/src/main/java/se2/carcassonne/model/Player.java
+++ app/src/main/java/se2/carcassonne/model/Player.java
@@ -11,23 +11,6 @@ public class Player {
    private Long id;
    private String username;
    private Long gameLobbyId;
-
-   @Override
-   public String toString() {
-      return "Player{" +
-              "id=" + id +
-              ", username='" + username + '\'' +
-              ", gameLobbyDto=" + gameLobbyId +
-              '}';
-   }
-
-   public String toJsonstring() {
-      return "{" +
-              "\"id\":" + id +
-              ", \"username\":\"" + username + '\"' +
-              ", \"gameLobbyDto\":" + gameLobbyId +
-              '}';
-   }
 }
 
 
