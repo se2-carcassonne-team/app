deea146b25bfc81c4e283bb2e245ec69ba3f66fe
Philipp Michael Arbeitstein <philippar@edu.aau.at>
2024-04-26 16:37:40 +0200

    refact: Some refactoring on the GameBoardActivity

--- app/src/main/java/se2/carcassonne/ui/GameBoardActivity.java
+++ app/src/main/java/se2/carcassonne/ui/GameBoardActivity.java
@@ -1,9 +1,7 @@
 package se2.carcassonne.ui;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,31 +9,28 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import se2.carcassonne.R;
+import se2.carcassonne.databinding.GameboardActivityBinding;
+import se2.carcassonne.helper.resize.FullscreenHelper;
 import se2.carcassonne.model.Tile;
 
 
 public class GameBoardActivity extends AppCompatActivity {
-
+    GameboardActivityBinding binding;
     private static final int ROWS = 30;
     private static final int COLS = 30;
 
     private GridView gridView;
     private GameboardAdapter gameboardAdapter;
     private Tile tile;
-    private TextView textViewRoutineId;
     private ImageView gameplay_card;
 
-    private void updateRotationIdTextView() {
-        TextView textViewRoutineId = findViewById(R.id.textViewRoutineId);
-        textViewRoutineId.setText("Routine ID: " + tile.getRotation());
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gameboard_activity);
-
-        setupFullscreen();
+        FullscreenHelper.setFullscreenAndImmersiveMode(this);
 
         gridView = findViewById(R.id.gridview);
         gridView.setScaleX(3.5f);
@@ -45,30 +40,16 @@ public class GameBoardActivity extends AppCompatActivity {
 
 
 
-        setupRotationButtons(gameplay_card);
+        setupRotationButtons();
         tile = new Tile(0L,"castle_wall_road", new int[]{3,2,1,2}, new int[]{3,3,3, 2,2,2, 1,1,1});
-        updateRotationIdTextView();
 
-        //ZoomFunction zoomfunktion = new ZoomFunction(this);
-        //gridView.setOnTouchListener(zoomfunktion);
-        ZoomFunction zoomFunction = new ZoomFunction(this, gridView);
-        gridView.setOnTouchListener(zoomFunction);
-
-
-
+        // ZoomFunction zoomFunction = new ZoomFunction(this, gridView);
+        // gridView.setOnTouchListener(zoomFunction);
 
     }
 
 
-    private void setupFullscreen() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-        );
-    }
-
-    private void setupRotationButtons(ImageView imageView) {
+    private void setupRotationButtons() {
         ImageView playingCard = findViewById(R.id.gameplay_card);
         final ImageRotator imageRotator = new ImageRotator(playingCard);
 
@@ -77,7 +58,6 @@ public class GameBoardActivity extends AppCompatActivity {
             public void onClick(View v) {
                 imageRotator.rotateRight();
                 tile.rotate(true);
-                updateRotationIdTextView();
             }
         });
 
@@ -86,12 +66,8 @@ public class GameBoardActivity extends AppCompatActivity {
             public void onClick(View v) {
                 imageRotator.rotateLeft();
                 tile.rotate(false);
-                updateRotationIdTextView();
             }
         });
-
-
-
     }
 }
 
